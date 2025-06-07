3759088cd1344fc8b155bd69570fa3d0d5da8273
yogeshOzaDeanInfotech <o.yogesh@deaninfotech.com>
2025-06-07 13:37:32 +0530

    backend url updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 function App() {
     const [usersData, setusersData] = useState([]);
     const getData = async () => {
-        const res = await axios.get("http://localhost:4000/api/users");
+        const res = await axios.get("http://43.204.219.136:4000/api/users");
         setusersData(res.data.users);
     };
     useEffect(() => {
