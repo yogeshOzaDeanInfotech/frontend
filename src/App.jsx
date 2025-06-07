dd064b246c6d79aa7ea71bf7bb1f713f9af3602f
yogeshOzaDeanInfotech <o.yogesh@deaninfotech.com>
2025-06-07 11:57:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+    const [usersData, setusersData] = useState([]);
+    const getData = async () => {
+        const res = await axios.get("http://localhost:4000/api/users");
+        setusersData(res.data.users);
+    };
+    useEffect(() => {
+        getData();
+    }, []);
+    return (
+        <div>
+            <h1>Test 1</h1>
+            <h1>List of users</h1>
+            {usersData.map((user) => (
+                <div>
+                    <p>{user.name}</p>
+                    <p>{user.email}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default App;
